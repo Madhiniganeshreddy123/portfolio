4fffc44ffa74da94205efdf2069f88b5a6184ed1
Madhini Ganesh Reddy <madhiniganeshreddy@gmail.com>
2026-04-01 20:02:05 +0530

    Clear all images in database

--- core/management/commands/load_sample_data.py
+++ core/management/commands/load_sample_data.py
@@ -6,10 +6,8 @@ class Command(BaseCommand):
     help = "Load or update project and profile data"
 
     def handle(self, *args, **kwargs):
-        Project.objects.filter(title="Customer Behaviour Analysis").update(image="")
-        Project.objects.filter(title="Multimodal Sentiment Analysis").update(image="")
-        Project.objects.filter(title="Real-Time QR Code System").update(image="")
-
-        Profile.objects.update_or_create(id=1, defaults={"profile_image": ""})
+        # Clear all images first
+        Project.objects.all().update(image="")
+        Profile.objects.all().update(profile_image="")
 
         self.stdout.write(self.style.SUCCESS("Data updated successfully!"))
